c80769b5fbf17e9580c8d204073011bdae467801
Yakub <jacobshavel@gmail.com>
2017-07-03 16:22:01 +0200

    First Commit

---
+++ iX_Location/iX_Location/AppleMapsViewController.swift
@@ -0,0 +1,25 @@
+//
+//  AppleMapsViewController.swift
+//  iX_Location
+//
+//  Created by Jacob Shavel on 7/3/17.
+//  Copyright © 2017 Jacob Shavel. All rights reserved.
+//
+
+import UIKit
+
+class AppleMapsViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
